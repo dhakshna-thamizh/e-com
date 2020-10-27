188382c957684124ce29638fd336053613e1a1f7
Dhakshna <manidhakshna@yahoo.com>
2020-10-27 18:37:43 +0530

    get information in firebase using Auth

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import './App.css';
 import SignInAndSignUp from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './Pages/Shop/shop.component';
 import Header from './Components/header/header.component';
+import { auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component = {HomePage} />
-        <Route path='/shop' component = {ShopPage} />
-        <Route path='/signin' component = {SignInAndSignUp}/>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  componentDidMount() { 
+    auth.onAuthStateChanged( user => {
+      this.setState({ currentUser : user });
+      console.log(user);
+    });
+    
+  }
+
+   render(){
+     return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component = {HomePage} />
+          <Route path='/shop' component = {ShopPage} />
+          <Route path='/signin' component = {SignInAndSignUp}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
