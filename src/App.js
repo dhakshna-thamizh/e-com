2fcd3cf4f4a892af1c430316302dfa376c41578a
Dhakshna <manidhakshna@yahoo.com>
2020-10-28 15:43:56 +0530

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -10,28 +10,33 @@ import { auth} from './firebase/firebase.utils';
 class App extends React.Component {
   constructor(){
     super();
-
     this.state = {
       currentUser : null
     }
   }
 
-  componentDidMount() { 
-    auth.onAuthStateChanged( user => {
-      this.setState({ currentUser : user });
-      console.log(user);
-    });
-    
-  }
+  unsubcribeFromAuth = null;
 
+    componentDidMount() { 
+      this.unsubcribeFromAuth =  auth.onAuthStateChanged( user => {
+        this.setState({ currentUser : user });
+      
+      });
+    
+    }
+
+    componentWillUnmount() {
+      this.unsubcribeFromAuth();
+    }
+  
    render(){
      return (
       <div>
-        <Header />
+        <Header currentUser = {this.state.currentUser}/>
         <Switch>
           <Route exact path='/' component = {HomePage} />
           <Route path='/shop' component = {ShopPage} />
-          <Route path='/signin' component = {SignInAndSignUp}/>
+          <Route path='/signin' component  = {SignInAndSignUp}/>
         </Switch>
       </div>
     );
--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -49,14 +49,14 @@ class SignIn extends React.Component {
                      label= 'password'
                      required
                     />
-
+                    <div className= 'buttons'>
                     <CustomButton type= 'submit'>
                         Sign In
                     </CustomButton>
-                    <CustomButton onClick = {signInWithGoogle}>
+                    <CustomButton onClick = {signInWithGoogle} isGoogleSignIn>
                        {''} Sign In with Google{''}
                     </CustomButton>
-                    
+                    </div>
                 </form>
             </div>
         );
