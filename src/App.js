de75120e86ec7465b2cf9ce619bc40d2eb13c453
Dhakshna <manidhakshna@yahoo.com>
2020-11-24 14:15:27 +0530

    adding checkoutpage component and view empty item in dropdown component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Switch, Route, Redirect  } from 'react-router-dom';
+
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import HomePage from './Pages/Homepage/homepage.component';
-import './App.css';
+
 import SignInAndSignUp from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './Pages/Shop/shop.component';
 import Header from './Components/header/header.component';
+import CheckoutPage from './Pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+
+import './App.css';
 
 
 class App extends React.Component {
@@ -49,6 +54,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component = {HomePage} />
           <Route path='/shop' component = {ShopPage} />
+          <Route exact path='/checkout' component = {CheckoutPage} />
           <Route 
             exact 
             path='/signin' 
