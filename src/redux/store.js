e98344c589e4a38e89c00bbb85331f453a073c8f
Dhakshna <manidhakshna@yahoo.com>
2020-12-02 19:28:27 +0530

    install redux-persist and implemented store.js root-reducer.js, index.js stored card items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];//keep in mind this configuration can be viewed on redux documentation as well
 
-const store = createStore (rootReducer, applyMiddleware(...middlewares));
+export const store = createStore (rootReducer, applyMiddleware(...middlewares));
 
 // or const store = createStore (rootReducer, applyMiddleware(logger))
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
 
